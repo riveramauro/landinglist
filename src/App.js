dd17d94e5ccb1ba9082f85b73e80933c01fd6131
Mauricio Rivera <maurocol03@gmail.com>
2021-10-07 00:02:10 -0400

    Query to contenctful working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react'
+
+const { REACT_APP_CF_SPACE, REACT_APP_CF_TOKEN } = process.env;
+
+const query = `
+query {
+  linkCollection (where: {
+   enableField: true
+  } ) {
+    items {
+      title,
+      url,
+      enableField
+    }
+  }
+}`
+
 function App() {
+
+  const [linkList, setLinkList] = useState(null)
+
+  useEffect(() => {
+    window.fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_CF_SPACE}?access_token=${REACT_APP_CF_TOKEN}`,
+    {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query })
+    })
+    .then((response) => response.json())
+    .then((data, errors) => {
+      console.log(data, errors)
+      if(errors) console.log(errors)
+      if(data) setLinkList(data.data.linkCollection.items)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Links</h2>
+      <div>
+        <Link link={linkList} />
+      </div>  
     </div>
   );
+}
+
+function Link(props) {
+
+  console.log(props)
+
+  const links = props.link;
+
+  return(
+    <div className="links__container">
+        {links && links.map(link => (
+          <div className="links_link">
+            <a href={link.url}>{link.title}</a>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default App;
