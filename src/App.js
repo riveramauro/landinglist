b0ca80a83c4040390fb734c0c555afac74979443
Mauricio Rivera <maurocol03@gmail.com>
2022-05-06 10:52:25 -0400

    Contenful list id as part of configuration .env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Logo from "../src/images/ssshape.svg";
 
 import samplelinkJSON from './links.json';
 
-const { REACT_APP_CF_SPACE, REACT_APP_CF_TOKEN, REACT_APP_GA_MEASUREMENT_ID } = process.env;
+const { REACT_APP_CF_SPACE, REACT_APP_CF_TOKEN, REACT_APP_CF_LISTID, REACT_APP_GA_MEASUREMENT_ID } = process.env;
 
 if (REACT_APP_GA_MEASUREMENT_ID ){  
   ReactGA.initialize(REACT_APP_GA_MEASUREMENT_ID);
@@ -15,7 +15,7 @@ if (REACT_APP_GA_MEASUREMENT_ID ){
 
 const query = `
 query{
-  linkList(id: "7HhMcgjq7KWUzaieP8llOx") {
+  linkList(id: "${REACT_APP_CF_LISTID}") {
     nameOfList,
     linksOnListCollection {
       total,
